bd39c955e5f83d90d63f6bd3cadbad70212d724d
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-02-05 01:36:05 +0300

    Update CORS policy
    Update CORS policy

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+var cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 app.use(express.json({extended: true}))
+app.options('*', cors());
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/userupdate', require('./routes/update.routes.js'))
