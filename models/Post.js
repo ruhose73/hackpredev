9779bb40ced35c7d7fad9173ffb87bff9a32a48e
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-02-04 13:19:53 +0300

    update posts
    add posts to schema User
    add  owner to schema Post

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,10 @@
 const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
-    user: {type: String, required:true},    //создатель поста
     header: {type: String, required:true,unique: true},
     body: {type: String, required:true},
     date: {type: Date, default: Date.now},  //дата создания поста
-    owner: {type: Types.ObjectId, ref: 'User'}
+    owner: {type: Types.ObjectId, ref: 'User'}  //создатель поста
 })
 
 module.exports = model('Post', schema)
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,6 +5,7 @@ const config = require('config')
 const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const Post = require('../models/Post')
+const User = require('../models/User')
 const router = Router()
 
 router.post(
@@ -26,13 +27,16 @@ router.post(
 
             const {access_token, header, body} = req.body
             const decodedToken = jwt.verify(access_token, config.get('jwtSecret'));
-            console.log(decodedToken.userId)
             const candidateHeader = await Post.findOne({header})
             if (candidateHeader) {
                 return res.status(400).json({message: 'Такой заголовок уже существует'})
             }
-            const user = new Post({user:decodedToken.userId, header, body})
-            await user.save()
+
+            const post = new Post({user:decodedToken.userId, header, body,owner: decodedToken.userId})
+            await post.save()
+
+            const userpost = await User.findByIdAndUpdate(decodedToken.userId, {posts:post.id}, {new:true})
+            await userpost.save()
 
             res.status(201).json({message: 'Пост создан'})
 
@@ -40,6 +44,39 @@ router.post(
             res.status(500).json({message: 'Ошибка сервера. Создание поста'})
         }
     })
+
+router.post(
+    '/postupdate',
+    [
+        check('header', 'заголовок не должен быть пустым').exists(),
+        check('body', 'Тело не должно быть пустым').exists()
+    ],
+    async (req, res)=> {
+        try{
+
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некорректные данные'
+                })
+            }
+
+            const {access_token,post, header, body} = req.body
+
+            const decodedToken = jwt.verify(access_token, config.get('jwtSecret'));
+            const user = await User.findByIdAndUpdate(decodedToken.userId, {header:header}, {new:true})
+            await user.save()
+
+            res.status(201).json({message: 'Пользователь обновлен'})
+        }
+        catch (e)
+        {
+            res.status(500).json({message: 'Ошибка сервера. Смена номера'})
+        }
+    }
+)
+
 
 
 module.exports = router
