8362313239491c99ee7e034a23bc89cda354a93f
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-02-05 15:37:26 +0300

    interprise

--- routes/enterprise.routes.js
+++ routes/enterprise.routes.js
@@ -48,4 +48,67 @@ router.post(
         }
     })
 
+//получить все интерпрайзы
+router.get('/allinterpises', async (req, res) => {
+    try {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
+        const {access_token} = req.headers
+        console.log(access_token)
+        const decodedToken = jwt.verify(access_token, config.get('jwtSecret'));
+        console.log(decodedToken)
+
+        const interprises = await Interprise.find({})
+        res.status(201).json({interprises})
+
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+})
+
+
+//получить интерпрайз СВОЙ
+router.get('/myinterpise', async (req, res) => {
+    try {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
+        const {access_token} = req.headers
+        console.log(access_token)
+        const decodedToken = jwt.verify(access_token, config.get('jwtSecret'));
+        console.log(decodedToken)
+
+        const interprises = await Interprise.find({author:decodedToken.userId })
+        res.status(201).json({interprises})
+
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+})
+
+//получить все посты другого пользователя
+router.post(
+    '/alluserinterpises',
+    async (req, res)=> {
+        try{
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
+            console.log(req.body)
+            const {access_token, user_id} = req.body
+            const decodedToken = jwt.verify(access_token, config.get('jwtSecret'));
+            const interprises = await Interprise.find({ author:user_id })
+
+            res.status(201).json({interprises})
+        }
+        catch (e)
+        {
+            res.status(500).json({message: 'Ошибка сервера. Обновление поста'})
+        }
+    }
+)
+
+
+
 module.exports = router
